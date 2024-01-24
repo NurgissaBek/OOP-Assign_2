f27863b95922bc6a212f8325114ef6988977bcbf
Нургиса <230780@astanait.edu.kz>
2024-01-24 08:22:17 +0600

    Second init

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,10 @@
 public class Employee extends Person{
+    private double salary;
+    private String position;
     public Employee(){
+        super();
+    }
+    public Employee(String name,String surname,String position,double salary){
+
     }
 }
