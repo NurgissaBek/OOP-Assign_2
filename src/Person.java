e713f8df4ff1fa2880ac44a81e02b97e3b0982d0
Нургиса <230780@astanait.edu.kz>
2024-01-24 08:16:30 +0600

    Second init

---
+++ src/Person.java
@@ -0,0 +1,46 @@
+public abstract class Person implements Payable, Comparable<Person>{
+    private static int id_gen = 1;
+    private int id;
+    private String name;
+    private String surname;
+
+
+    public Person(){
+        this.id = id_gen++;
+    }
+    public Person(String name, String surname){
+        this();
+        setName(name);
+        setSurname(surname);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return id+". "+name+" "+surname;
+    }
+
+    public String getposition(){
+        return "Student";
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return 0;
+    }
+}
